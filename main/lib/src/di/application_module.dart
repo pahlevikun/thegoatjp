c5bd21da188db8b1561663fc7de0869f04ed54c8
Farhan Yuda Pahlevi <pahlevi.kun@gmail.com>
2022-12-15 02:55:46 +0700

    Clean-up remove unused dependencies

--- main/lib/src/di/application_module.dart
+++ main/lib/src/di/application_module.dart
@@ -1,15 +1,7 @@
-import 'package:alice/alice.dart';
 import 'package:thegoatjp/src/h.dart';
 
 @module
 abstract class ApplicationModules {
-  @LazySingleton()
-  ConnectionChecker provideConnectionChecker() =>
-      ConnectionChecker.getInstance();
-
-  @LazySingleton()
-  PermissionHelper providePermissionHelper() => PermissionHelper();
-
   @Singleton()
   @Named("build_mode")
   bool provideBuildMode() => BuildConfig.BUILD_STAGE_MODE && !kReleaseMode;
